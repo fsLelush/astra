4389816d6b6dfbc884fa2ec0c3eb6bfbd0106464
sergOfan <60229898+fsLelush@users.noreply.github.com>
2024-03-28 11:35:02 +0700

    Поправил недоделанные ip

--- Scripts/hosts.sh
+++ Scripts/hosts.sh
@@ -1,3 +1,3 @@
-echo "192.168.140 tmk.kuzdrav.ru" >> /etc/hosts
-echo "192.168.140 tmk.42kuzdrav.ru" >> /etc/hosts
+echo "192.168.42.140 tmk.kuzdrav.ru" >> /etc/hosts
+echo "192.168.42.140 tmk.42kuzdrav.ru" >> /etc/hosts
 exit
